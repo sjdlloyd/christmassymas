6251b5e0e3a96b7f27b607f43528b56c6cd3a987
sjdlloyd <sjdlloyd@live.co.uk>
2018-12-23 22:08:02 +1300

    initial

---
+++ christmassymas/__init__.py
@@ -0,0 +1,2 @@
+def hohoho():
+    print('hohohohohoh ho...')
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='christmassymas',
+      version='0.11',
+      description='The christmassiest in the world',
+      url='http://github.com/sjdlloyd/christmassymas',
+      author='Sam Lloyd',
+      author_email='sjdlloyd@live.co.uk',
+      license='MIT',
+      packages=['christmassymas'],
+      zip_safe=False)
